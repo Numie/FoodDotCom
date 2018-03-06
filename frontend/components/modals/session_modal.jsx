b9070887c946f0adb01845c39b5bd84892e186d7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-06 16:53:35 -0500

    Set up for push to Heroku

--- frontend/components/modals/session_modal.jsx
+++ frontend/components/modals/session_modal.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 
-const SessionModal = (props) => {
-  return (
-    <div className='modal'>
-      <img className='logo' src={window.staticImages.name} alt='FoodDotCom' />
-      <h2 className='heading'>Sign in with your Food.com account</h2>
-      <form className='session-form'>
-        <input type='email' placeholder='Email' value=''/>
-        <input type='password' placeholder='Password' value=''/>
-        <input type='submit' value='Sign In'/>
-        <input id='guest-login' type='submit' value='Guest Sign In'/>
-        <h6>Don't have an account? <a>Create your account</a></h6>
-      </form>
-    </div>
-  );
-};
+export default class SessionModal extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default SessionModal;
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.handlelogin = this.handlelogin.bind(this);
+  }
+
+  handlelogin(e) {
+    e.preventDefault();
+    this.props.login();
+  }
+
+  render() {
+    return (
+      <div className='modal'>
+        <img className='logo' src={window.staticImages.name} alt='FoodDotCom' />
+        <h2 className='heading'>Sign in with your Food.com account</h2>
+
+        <form className='session-form'>
+          <input type='email' placeholder='Email' value={this.state.email}/>
+          <input type='password' placeholder='Password' value={this.state.password}/>
+          <input type='submit' value='Sign In' onClick={this.handlelogin}/>
+          <input id='guest-login' type='submit' value='Guest Sign In'/>
+          <h6>Don't have an account? <a>Create your account</a></h6>
+        </form>
+
+        <button className="x-close" onClick={this.props.toggleModal}>x</button>
+      </div>
+    );
+  }
+}

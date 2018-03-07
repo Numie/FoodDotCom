6501d5c16685ef14d2b4c66b05919fcdb297532b
Jason Numeroff <jnumeroff@hotmail.com>
2018-03-06 23:54:18 -0500

    Begin combinding login and signup forms

--- frontend/actions/modal_actions.js
+++ frontend/actions/modal_actions.js
@@ -1,5 +1,10 @@
 export const SESSION_MODAL = 'SESSION_MODAL';
+export const SIGNUP_MODAL = 'SIGNUP_MODAL';
 
-export const toggleModal = () => ({
+export const toggleSessionModal = () => ({
   type: SESSION_MODAL
+});
+
+export const toggleSignupModal = () => ({
+  type: SIGNUP_MODAL
 });
--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Greeting from './greeting';
 import { logout } from '../../actions/session_actions';
-import { toggleModal } from '../../actions/modal_actions';
+import { toggleSessionModal } from '../../actions/modal_actions';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => ({
   currentUser: state.session.currentUser,
-  sessionModal: state.ui.modals.sessionModal
+  sessionModal: state.ui.modals.sessionModal,
+  signupModal: state.ui.modals.signupModal
 });
 
 const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logout()),
-  toggleModal: () => dispatch(toggleModal())
+  toggleSessionModal: () => dispatch(toggleSessionModal())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Greeting);

8723b78b6348e7f69c6d9b25ed10981d0e93d70c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-07 14:16:59 -0500

    Add and style session errors

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+export const CLEAR_SESSION_ERRORS = 'CLEAR_SESSION_ERRORS';
 
 export const receiveCurrentUser = (user) => ({
   type: RECEIVE_CURRENT_USER,
@@ -11,6 +12,10 @@ export const receiveCurrentUser = (user) => ({
 export const receiveErrors = (errors) => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_SESSION_ERRORS
 });
 
 export const signup = (user) => dispatch => {
--- frontend/components/modals/session_modal_container.js
+++ frontend/components/modals/session_modal_container.js
@@ -1,20 +1,22 @@
 import React from "react";
 import SessionModal from './session_modal';
-import { login, signup } from '../../actions/session_actions';
+import { login, signup, clearErrors } from '../../actions/session_actions';
 import { toggleSessionModal } from '../../actions/modal_actions';
 import { toggleSignupModal } from '../../actions/modal_actions';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
   sessionModal: state.ui.modals.sessionModal,
-  signupModal: state.ui.modals.signupModal
+  signupModal: state.ui.modals.signupModal,
+  errors: state.errors.session.responseJSON
 });
 
 const mapDispatchToProps = dispatch => ({
   login: (user) => dispatch(login(user)),
   signup: (user) => dispatch(signup(user)),
   toggleSessionModal: () => dispatch(toggleSessionModal()),
-  toggleSignupModal: () => dispatch(toggleSignupModal())
+  toggleSignupModal: () => dispatch(toggleSignupModal()),
+  clearErrors: () => dispatch(clearErrors())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionModal);

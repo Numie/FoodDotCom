ff641e0a84ee027016e22bb029a4649c8dcbc69e
Jason Numeroff <jnumeroff@hotmail.com>
2018-03-07 20:14:49 -0500

    Implement Google address autocomplete and search bar styling

--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -4,9 +4,11 @@ import RestaurantSearchForm from './restaurant_search/restaurant_search_form';
 const Main = () => {
   return (
     <div className='main'>
-      <h1>Who delivers in your neighborhood?</h1>
-      <div><h6>Start your order now.</h6></div>
-      <RestaurantSearchForm />
+      <div className='main-inner-container'>
+        <h1>Who delivers in your neighborhood?</h1>
+        <div><h6>Start your order now.</h6></div>
+        <RestaurantSearchForm />
+      </div>
     </div>
   );
 };

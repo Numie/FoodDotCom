5e0c7c106815d4cde62bd49e23db4aff92d64b79
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-09 12:28:21 -0500

    Add search bar to all pages and make address persist between pages

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { AddressRequiredRoute } from '../util/route_util';
 import { Link } from 'react-router-dom';
+import RestaurantSearchFormContainer from './restaurant_search/restaurant_search_form_container';
 import GreetingContainer from './greeting/greeting_container';
 
 const Header = (props) => {
   return (
     <header className='header'>
-      <Link to='/'>
-        <img className='logo' src={window.staticImages.logo} alt='FoodDotCom' />
-      </Link>
+      <div className='header-left'>
+        <Link to='/'>
+          <img className='logo' src={window.staticImages.logo} alt='FoodDotCom' />
+        </Link>
+        <AddressRequiredRoute path='/(.+)' component={RestaurantSearchFormContainer} />
+      </div>
       <GreetingContainer />
     </header>
   );

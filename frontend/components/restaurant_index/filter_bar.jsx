a0e04ea62d805468d1c9f9b1d0d58815ff6420da
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-08 19:27:39 -0500

    Add basic restaurant index styling

---
+++ frontend/components/restaurant_index/filter_bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class RestaurantIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='filter-bar'>
+
+      </div>
+    );
+  }
+}

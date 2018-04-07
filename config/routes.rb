7b21ec8e3e07a047946c0e238acbbe1195ff6c84
Jason Numeroff <jnumeroff@hotmail.com>
2018-04-07 11:02:19 -0400

    Remove order items controller, which is unnecessary due to orders accepting nested attributes

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,9 @@ Rails.application.routes.draw do
     resources :restaurants, only: [:index]
     resources :restaurants, only: [:show] do
       resources :menu_items, only: [:index]
-      # resources :item_option_sections, only: [:index]
-      # resources :item_options, only: [:index]
       resources :reviews, only: [:index, :create]
     end
     resources :orders, only: [:create] do
-      resources :order_items, only: [:create]
-      resources :order_item_options, only: [:create]
     end
   end
 end

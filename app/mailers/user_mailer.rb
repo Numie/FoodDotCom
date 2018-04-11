22c9bb612e14b661b9a7d4f69288cd791ae0551a
Jason <jnumeroff@hotmail.com>
2018-04-11 19:25:14 -0400

    Finalize email layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,9 @@ class UserMailer < ApplicationMailer
     @email = email
     @order = order
     @items = items.values
+
+    attachments.inline['logo2.jpg'] = File.read('app/assets/images/logo2.jpg')
+
     mail(to: email, subject: 'Thanks for your (fake) order!')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@food.com'}
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,

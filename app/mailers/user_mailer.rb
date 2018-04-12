c5e7edb6ca3a9855625b52b5172dd9c7df871478
Jason <jnumeroff@hotmail.com>
2018-04-12 13:39:19 -0400

    Host logo image on imgur to bypass sending an attachment with email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,6 @@ class UserMailer < ApplicationMailer
     @order = order
     @items = items.values
 
-    attachments.inline['logo2.jpg'] = File.read('app/assets/images/logo2.jpg')
-
     mail(from: 'no-reply@food.com', to: email, subject: 'Thanks for your (fake) order!')
   end
 end

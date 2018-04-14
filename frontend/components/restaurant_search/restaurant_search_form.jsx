81264b1b7c2a7bff69be265958910b1b85ed8cef
Jason Numeroff <jnumeroff@hotmail.com>
2018-04-14 19:05:19 -0400

    Fix bug that incorrectly displayed an error message when change address on click

--- frontend/components/restaurant_search/restaurant_search_form.jsx
+++ frontend/components/restaurant_search/restaurant_search_form.jsx
@@ -23,25 +23,22 @@ export default class RestaurantSearchForm extends React.Component {
     };
     const autocomplete = new google.maps.places.Autocomplete(this.searchBar, options);
     autocomplete.addListener('place_changed', (arg) => {
-      debugger
-      this.handleSubmitOnSelection();
       this.setState({address: autocomplete.getPlace().formatted_address});
+      this.handleSubmitOnSelection();
     });
   }
 
   handleSubmitOnSelection() {
-    debugger
     if (this.props.location.pathname !== '/') {
-      debugger
       this.props.fetchGeocode(this.state.address).then(() => {
-        debugger
-        if (this.props.address.addressComponents[0].types.includes("street number")) {
-          debugger
+        if (this.props.address.addressComponents[0].types.includes("street_number")) {
           this.props.fetchRestaurants(this.state.address).then(() => {
             if (this.props.restaurants.length === 0) {
               this.props.receiveErrors();
             } else {
-              this.props.history.push('/restaurants');
+              if (this.props.location.pathname !== '/restaurants') {
+                this.props.history.push('/restaurants');
+              }
             }
           });
         } else {

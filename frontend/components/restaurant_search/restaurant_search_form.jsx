95458c4d6c72b77d0fd55964340a0c9db0087125
Jason Numeroff <jnumeroff@hotmail.com>
2018-04-14 23:55:07 -0400

    Work with Google autocomplete default functionality to allow searches in many edge case scenarios

--- frontend/components/restaurant_search/restaurant_search_form.jsx
+++ frontend/components/restaurant_search/restaurant_search_form.jsx
@@ -4,11 +4,13 @@ export default class RestaurantSearchForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: this.props.address ? this.props.address.formattedAddress : ""
+      address: this.props.address ? this.props.address.formattedAddress : "",
+      enter: false
     };
     this.clearAddress = this.clearAddress.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSubmitOnSelection = this.handleSubmitOnSelection.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
 
   componentDidMount() {
@@ -23,46 +25,78 @@ export default class RestaurantSearchForm extends React.Component {
     };
     const autocomplete = new google.maps.places.Autocomplete(this.searchBar, options);
     autocomplete.addListener('place_changed', (arg) => {
-      this.setState({address: autocomplete.getPlace().formatted_address});
-      this.handleSubmitOnSelection();
+      let address = autocomplete.getPlace();
+      if (address.formatted_address) {
+        this.setState({address: address.formatted_address}, () => {
+          if (this.state.enter) {
+            this.setState({enter: false});
+            this.handleSubmit();
+          }
+        });
+      } else {
+        this.setState({address: address.name}, () => {
+          this.handleSubmit();
+        });
+      }
+      if (this.props.location.pathname !== '/') {
+        this.handleSubmitOnSelection();
+      }
     });
   }
 
   handleSubmitOnSelection() {
-    if (this.props.location.pathname !== '/') {
-      this.props.fetchGeocode(this.state.address).then(() => {
-        if (this.props.address.addressComponents[0].types.includes("street_number")) {
-          this.props.fetchRestaurants(this.state.address).then(() => {
-            if (this.props.restaurants.length === 0) {
-              this.props.receiveErrors();
-            } else {
-              if (this.props.location.pathname !== '/restaurants') {
-                this.props.history.push('/restaurants');
-              }
-            }
-          });
-        } else {
-          this.props.receiveErrors();
-        }
-      });
-    }
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
     this.props.fetchGeocode(this.state.address).then(() => {
       if (this.props.address.addressComponents[0].types.includes("street_number")) {
-        this.props.fetchRestaurants(this.state.address).then(() => {
+        this.props.fetchRestaurants(this.props.address.formattedAddress).then(() => {
           if (this.props.restaurants.length === 0) {
+            if (this.state.enter) {
+              this.setState({enter: false});
+            }
             this.props.receiveErrors();
           } else {
-            this.props.history.push('/restaurants');
+            if (this.props.location.pathname !== '/restaurants') {
+              this.props.history.push('/restaurants');
+            }
           }
         });
       } else {
+        if (this.state.enter) {
+          this.setState({enter: false});
+        }
         this.props.receiveErrors();
       }
     });
+  }
+
+  handleSubmit(e) {
+    if (e) e.preventDefault();
+    this.props.fetchGeocode(this.state.address).then(() => {
+      if (this.props.address.addressComponents[0].types.includes("street_number")) {
+        this.props.fetchRestaurants(this.props.address.formattedAddress).then(() => {
+          if (this.props.restaurants.length === 0) {
+            if (this.state.enter) {
+              this.setState({enter: false});
+            }
+            this.props.receiveErrors();
+          } else {
+            if (this.props.location.pathname !== '/restaurants') {
+              this.props.history.push('/restaurants');
+            }
+          }
+        });
+      } else {
+        if (this.state.enter) {
+          this.setState({enter: false});
+        }
+        this.props.receiveErrors();
+      }
+    });
+  }
+
+  handleEnter(e) {
+    if (e.key === 'Enter') {
+      this.setState({enter: true});
+    }
   }
 
   update(field) {
@@ -80,7 +114,8 @@ export default class RestaurantSearchForm extends React.Component {
 
     return(
       <form className={this.props.location.pathname === '/checkout' ? 'hidden' : 'address-search-form'} onSubmit={this.handleSubmit}>
-        <input ref={(input) => { this.searchBar = input; }} className='address-search-field' type='text' placeholder='Enter your address (NYC only!)' value={this.state.address} onChange={this.update('address')} onClick={this.props.error !== "" ? this.props.clearErrors : null} />
+        <input ref={(input) => { this.searchBar = input; }} className='address-search-field' type='text' placeholder='Enter your address (NYC only!)'
+          value={this.state.address} onChange={this.update('address')} onClick={this.props.error !== "" ? this.props.clearErrors : null} onKeyPress={this.handleEnter}/>
         {this.state.address? <button className="x-close" onClick={this.clearAddress}>&times;</button> : null }
         <input type='submit' value='Find food' />
         {error ? <div className='address-error-box'>{error}</div> : null}
